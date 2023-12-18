7e9f6369a1cd02e9ac0a7e736dbab04a38d5bfb3
Siva kumar Ch <siva@Sivas-Air.attlocal.net>
2023-12-17 21:54:56 -0600

    payservice changes

--- src/main/java/com/user/payservice/PayserviceApplication.java
+++ src/main/java/com/user/payservice/PayserviceApplication.java
@@ -2,9 +2,18 @@ package com.user.payservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class PayserviceApplication {
+
+	@GetMapping("/hello")
+	public String sayHello()
+	{
+		return "Hello! from pay service";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PayserviceApplication.class, args);
