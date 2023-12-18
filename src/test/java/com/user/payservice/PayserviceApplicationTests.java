c2ec261323985c04f3de979fa20ca09c9949228e
Siva kumar Ch <siva@Sivas-Air.attlocal.net>
2023-12-17 20:41:51 -0600

    payservice changes

---
+++ src/test/java/com/user/payservice/PayserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.user.payservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
